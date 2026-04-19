8977c58ebf5478202effa9f6a3a6a441b2e1ed07
Ronald Dias <ronaldsilva850@gmail.com>
2026-04-19 15:44:49 -0300

    chore: remove express and bootstrap nestjs architecture

--- src/infra/cryptography/Argon2Adapter.ts
+++ src/infra/cryptography/Argon2Adapter.ts
@@ -1,19 +1,14 @@
+import { Injectable } from "@nestjs/common";
 import * as argon2 from "argon2";
-import { Hasher } from "../../application/protocols/cryptography/Hasher";
+import { Hasher } from "src/application/protocols/cryptography/Hasher";
 
+@Injectable()
 export class Argon2Adapter implements Hasher {
-  private readonly config = {
-    type: argon2.argon2id,
-    memoryCost: 2 ** 16,
-    timeCost: 3,
-    parallelism: 1,
-  };
+    public async hash(plaintext: string): Promise<string> {
+        return argon2.hash(plaintext);
+    }
 
-  public async hash(plaintext: string): Promise<string> {
-    return argon2.hash(plaintext, this.config);
-  }
-
-  public async compare(plaintext: string, digest: string): Promise<boolean> {
-    return argon2.verify(digest, plaintext);
-  }
+    public async compare(plaintext: string, digest: string): Promise<boolean> {
+        return argon2.verify(digest, plaintext);
+    }
 }
